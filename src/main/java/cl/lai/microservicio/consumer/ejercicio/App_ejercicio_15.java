fe5d4fa62c0c15a75b2c1f4a1a493ad3ea2328f7
Chun Hau Lai <c.lai@adevcom.cl>
2018-09-11 12:53:57 -0300

    Agregar ejercicio 15

---
+++ src/main/java/cl/lai/microservicio/consumer/ejercicio/App_ejercicio_15.java
@@ -0,0 +1,18 @@
+package cl.lai.microservicio.consumer.ejercicio;
+
+
+/**
+ * @author chunhaulai
+ *
+ */
+public class App_ejercicio_15 {
+    public static void main( String[] args ){
+    	//1. Definir el controller BancoController que utiliza el servicio de SERVICE-CATEGORIA-BANCO
+    	//2. Implementar el metodo /banco que retorna http://SERVICE-CATEGORIA-BANCO/banco/list.htm
+    	//3. Levantar AppEurekaServer del proyecto spring-taller-microservicio-eureka
+    	//4. Levantar App_tutorial_15 del proyecto spring-taller-manager
+    	//5. Levantar cl.lai.microservicio.consumer.App_tutorial_15 del proyecto spring-taller-microservicio-consumer
+    	
+
+    }
+}
